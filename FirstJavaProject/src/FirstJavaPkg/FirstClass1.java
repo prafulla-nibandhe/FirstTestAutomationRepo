9ebf1ccce5a159074d26d5e4b05761fdd8bd0beb
prafulla-nibandhe <prafullanibandhe@gmail.com>
2024-04-30 17:08:11 -0500

    this is my first commit

---
+++ FirstJavaProject/src/FirstJavaPkg/FirstClass1.java
@@ -0,0 +1,13 @@
+package FirstJavaPkg;
+
+public class FirstClass1 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Gana Gana Ganat Bote!");
+		System.out.println("Gajanan Maharaj Ki Jay!!");
+		System.out.println(9+23);
+		System.out.println();
+		
+	}
+}
